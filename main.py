649a2896608ee3fa03e458b27480438c1a2a6279
Kazper91 <kazper91@gmail.com>
2024-03-20 22:27:06 +0300

    refactor: test python built-in functions

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello Igor!")
+name = 'Igor';
+print(f'Hello {name}');
+print(10, 'Igor', True);
+print(dir()); #возвращает список всех атрибутов объекта
+print(dir(__builtins__)); #вывод встроенных в Python функций
+input("Enter your name:") #input запрос у пользователя ввода данных. Ввод данных не через интерпретатор Visual Code Studios
