83c6115e4c8bcfe729ddfc07dca2b2621845163d
kazper91@gmail.com <kazper91@gmail.com>
2024-03-26 21:25:54 +0300

    feat: variable identification test

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import datetime  # импорт модуля datetime
+string_data = 'some text'
 
 
-def hello(name):
-    print("Hello there,", name)
-    print("Hi there,", name)
+def add_letters(string):
+    string = string + ' new text'
+    return string
 
 
-hello('Igor')
+print(add_letters(string_data))
 
-
-def sum_nums(a, b):
-    sum = a + b
-    return sum
-
-
-first_sum = sum_nums(10, 5)
-
-print(first_sum)
-
-print(sum_nums(50.5, 20))
-
-print(sum_nums(sum_nums(50.5, 20), 30))
-
-print(datetime.MAXYEAR)
+print(string_data)
