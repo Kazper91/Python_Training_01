bf4f9a5bd5dd09817bcc8f4aaf83691cd59bff18
kazper91@gmail.com <kazper91@gmail.com>
2024-03-26 19:06:53 +0300

    feat: function test and module import test

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import datetime  # импорт модуля datetime
+
+
 def hello(name):
     print("Hello there,", name)
     print("Hi there,", name)
 
 
 hello('Igor')
+
+
+def sum_nums(a, b):
+    sum = a + b
+    return sum
+
+
+first_sum = sum_nums(10, 5)
+
+print(first_sum)
+
+print(sum_nums(50.5, 20))
+
+print(sum_nums(sum_nums(50.5, 20), 30))
+
+print(datetime.MAXYEAR)
